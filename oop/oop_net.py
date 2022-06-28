d5cd48ecb16d587ab46e291a7c1ffd210244381a
aoscoma <chriss_kolle@mail.ru>
2022-06-28 15:15:06 +0300

    oop

--- oop/oop_net.py
+++ oop/oop_net.py
@@ -5,6 +5,14 @@
 # Очевидно, имя, фамилия и список закрепленных курсов логично реализовать на уровне родительского класса. 
 # А чем же будут специфичны дочерние классы? Об этом в следующих заданиях.
 
+
+# В квизе к предыдущей лекции мы реализовали возможность выставлять студентам оценки за домашние задания. 
+# Теперь это могут делать только Reviewer (реализуйте такой метод)! А что могут делать лекторы? 
+# Получать оценки за лекции от студентов :) Реализуйте метод выставления оценок лекторам у класса Student 
+# (оценки по 10-балльной шкале, хранятся в атрибуте-словаре у Lecturer, в котором ключи – названия курсов, а значения – списки оценок). 
+# Лектор при этом должен быть закреплен за тем курсом, на который записан студент.
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -20,15 +28,20 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+lecturer = Mentor()
+
+reviewer = Mentor()
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+            if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
             else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
- 
+                return 'Ошибка'
+
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
